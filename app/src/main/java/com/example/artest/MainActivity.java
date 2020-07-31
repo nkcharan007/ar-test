e910384cf62fd251ae7e3322427870d06ee3083b
nkcharan007 <40414538+nkcharan007@users.noreply.github.com>
2020-07-31 22:53:56 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/artest/MainActivity.java
+++ app/src/main/java/com/example/artest/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.artest;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.ArSceneView;
 import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
@@ -21,13 +37,30 @@ import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE = 1234;
+    public static String CAMERA_POSITION_FRONT;
+    public static String CAMERA_POSITION_BACK;
+    public static String MAX_ASPECT_RATIO;
+
+
+
     private Button removeBtn;
     private ArFragment arFragment;
+    private  Button capture;
+    private Button show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +70,8 @@ public class MainActivity extends AppCompatActivity {
         FirebaseApp.initializeApp(this);
 
         removeBtn = findViewById(R.id.remove_btn);
+        capture = findViewById(R.id.capture);
+        show = findViewById(R.id.display);
 
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference modelRef = storage.getReference().child("out.glb");
@@ -89,6 +124,25 @@ public class MainActivity extends AppCompatActivity {
             });
 
         });
+
+        capture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(checkPermission()) {
+                    TakeImage();
+                }
+            }
+        });
+
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DisplayImage();
+            }
+        });
+        checkPermission();
         ////// updated transformable node
 
 
@@ -113,6 +167,117 @@ public class MainActivity extends AppCompatActivity {
 //        transformableNode.select();
 //    }
         ////// reference
+
+    }
+
+
+
+
+    private String generateFilename() {
+        String date = new SimpleDateFormat("yyyyMMddHHmmss", java.util.Locale.getDefault()).format(new Date());
+        File mediaStorageDir = new File(Environment.getExternalStorageDirectory(), "Sceneform");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("AR App Error", "failed to create directory");
+                return null;
+            }
+        }
+
+        return mediaStorageDir.getAbsoluteFile() + File.separator + date + "_screenshot.jpg";
+    }
+
+     private boolean checkPermission() {
+         int result;
+         List<String> listPermissionsNeeded = new ArrayList<>();
+         String[] permissions = new String[] {
+                 Manifest.permission.READ_EXTERNAL_STORAGE,
+                 Manifest.permission.WRITE_EXTERNAL_STORAGE
+         };
+         for (String p : permissions) {
+             result = ContextCompat.checkSelfPermission(this, p);
+             if (result != PackageManager.PERMISSION_GRANTED) {
+                 listPermissionsNeeded.add(p);
+             }
+         }
+         if (!listPermissionsNeeded.isEmpty()) {
+             ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
+             return false;
+         }
+         return true;
+     }
+
+    private void saveBitmapToDisk(Bitmap bitmap, String filename) throws IOException {
+
+        File out = new File(filename);
+        if (!out.getParentFile().exists()) {
+            out.getParentFile().mkdirs();
+        }
+        try (FileOutputStream outputStream = new FileOutputStream(filename);
+             ByteArrayOutputStream outputData = new ByteArrayOutputStream()) {
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData);
+            outputData.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException ex) {
+            throw new IOException("Failed to save bitmap to disk", ex);
+        }
+    }
+
+
+    private void TakeImage() {
+        final String filename = generateFilename();
+        ArSceneView view = arFragment.getArSceneView();
+
+        // Create a bitmap the size of the scene view.
+        final Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // Create a handler thread to offload the processing of the image.
+        final HandlerThread handlerThread = new HandlerThread("PixelCopier");
+        handlerThread.start();
+        // Make the request to copy.
+        PixelCopy.request(view, bitmap, (copyResult) -> {
+            if (copyResult == PixelCopy.SUCCESS) {
+                try {
+                    saveBitmapToDisk(bitmap, filename);
+                } catch (IOException e) {
+                    Toast toast = Toast.makeText(MainActivity.this, e.toString(),
+                            Toast.LENGTH_LONG);
+                    toast.show();
+                    return;
+                }
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),
+                        "Photo saved", Snackbar.LENGTH_LONG);
+                snackbar.setAction("Open in Photos", v -> {
+                    File photoFile = new File(filename);
+
+                    Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                            MainActivity.this.getPackageName() + ".ar.provider",
+                            photoFile);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, photoURI);
+                    intent.setDataAndType(photoURI, "image/*");
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    startActivity(intent);
+
+                });
+                snackbar.show();
+            } else {
+                Toast toast = Toast.makeText(MainActivity.this,
+                        "Failed to copyPixels: " + copyResult, Toast.LENGTH_LONG);
+                toast.show();
+            }
+            handlerThread.quitSafely();
+        }, new Handler(handlerThread.getLooper()));
+    }
+
+
+
+    private void DisplayImage() {
+
+        Intent intent = new Intent(getApplicationContext(),GetImages.class);
+        startActivity(intent);
+
 
     }
 
